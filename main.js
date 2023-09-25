00f744cbb3c45bc624f0d15b6fb13f6bb3aca609
Myhrindra194 <myhrindra194@gmail.com>
2023-09-25 10:48:38 +0300

    Progress circle bar

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let circularProgress = document.querySelectorAll(".circular-progress");
 let progressValue = document.querySelectorAll(".progress-value");
    
@@ -6,6 +5,7 @@ for(let i =0 ; i < progressValue.length; i++){
     progressValue[i].textContent =  `${progressValue[i].dataset.value}%`;
     circularProgress[i].style.background = `conic-gradient(#7d2ae8 ${progressValue[i].dataset.value * 3.6}deg , #ededed 0deg)`;
 }
+
 window.addEventListener("load" , () => {
     document.querySelector(".preloader").classList.add("preloader-hide");
 })
