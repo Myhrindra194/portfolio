f75c7437d0280cb8ce360260bfa5e79ec366dc03
myhrindra194 <myhrindra194@gmail.com>
2024-07-21 21:54:18 +0300

    Second version of my portfolio

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-let circularProgress = document.querySelectorAll(".circular-progress");
-let progressValue = document.querySelectorAll(".progress-value");
-   
-for(let i =0 ; i < progressValue.length; i++){
-    progressValue[i].textContent =  `${progressValue[i].dataset.value}%`;
-    circularProgress[i].style.background = `conic-gradient(#7d2ae8 ${progressValue[i].dataset.value * 3.6}deg , #ededed 0deg)`;
-}
+const navbarClone = document.querySelector("nav").cloneNode(true);
 
-window.addEventListener("load" , () => {
-    document.querySelector(".preloader").classList.add("preloader-hide");
+navbarClone.classList.add("fixed-top", "bg-white", "border-bottom");
+
+window.addEventListener("scroll", onscroll = () => {
+    if (window.scrollY > 0)
+        document.querySelector("body").prepend(navbarClone);
+    else
+        navbarClone.remove()
 })
